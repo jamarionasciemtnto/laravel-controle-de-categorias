1f094837c39f633b85dcd9c5b8af3ac079d54ac0
jamario <jamariobatista@gmail.com>
2020-02-22 15:51:28 -0300

    aplicaçãcao router fail

--- public/js/app.js
+++ public/js/app.js
@@ -19335,8 +19335,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\teste\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\teste\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Dev\laravel\laravel-controle-de-categorias\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Dev\laravel\laravel-controle-de-categorias\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
